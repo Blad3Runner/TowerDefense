adb7ae03b641fcb6e44db97f7e72141fb12ad156
Alex <32169060+Blad3Runner@users.noreply.github.com>
2026-08-20 12:32:02 +0200

    Migrate to UE 5.8
    - Project upgraded from 5.7.4 to 5.8
    - Removed vendored plugin copies; BlueprintAssist and ElectronicNodes
      now installed via Fab to the engine
    - Asset files reserialised by the new engine version

--- Source/TowerDefenseEditor.Target.cs
+++ Source/TowerDefenseEditor.Target.cs
@@ -1,15 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
-
 public class TowerDefenseEditorTarget : TargetRules
 {
-	public TowerDefenseEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-
-		ExtraModuleNames.AddRange( new string[] { "TowerDefense" } );
-	}
+    public TowerDefenseEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
+        ExtraModuleNames.AddRange(new string[] { "TowerDefense" });
+    }
 }
